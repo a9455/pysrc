d281bf920f5df6b6f1245d4e0c82f10f96bcc140
birdy <birdy1617@gmail.com>
2022-06-01 17:53:47 +0900

    update

--- Operations.py
+++ Operations.py
@@ -67,40 +67,77 @@ class OperationNetCDF():
 		
 		# 次元数の設定
 		# ---------------------------
-		for key in list(DictionaryData.keys):
+		for key in list(DictionaryData.keys()):
 			tmp	= DictionaryData[key]
 			if tmp[0] == "axis":
-				nc.creatDimension(key, tmp[1])
+				NcData.createDimension(key, tmp[1])
 		
 		# データの設定
 		# ---------------------------
-		for key in list(DictionaryData.keys):
+		for key in list(DictionaryData.keys()):
 			tmp	= DictionaryData[key]
 			if tmp[0] == "data":
-				print(key, tmp[1], tmp[2])
-				tmpVar		= nc.creatDimension(key, tmp[1], tuple(tmp[2]), fill_value=-9999)
+				tmpVar		= NcData.createVariable(key, tmp[1], tuple(tmp[2]), fill_value=-9999)
 				tmpVar[:,:]	= tmp[3]
 
-		nc.close()
-
-
-
+		NcData.close()
 
 
 
 class OperationPandas():
-	def __init__(self, df1):
-		self.df1	= df1		# 編集したいデータフレーム
+	# 単一のデータフレームを編集
+	# -----------------------------------------------------
+	def load_df_from_df(self, df):
+		""" 既に定義したデータフレームを選択する """
+		self.df1	= df
+	
+	def load_df_from_filename(self, Filename:str, header=True, index_col=0):
+		""" 新規ファイルを読み込む """
+		self.df1	= pd.read_csv(Filename, header=header, index_col=index_col)
 
-	def Str2Numeric(self, Header:str):
+	def reset_header(self, NewHeader:list):
+		""" ヘッダーの再定義 """
+		self.df1.columns	= NewHeader
+
+	def str_to_numeric(self, Header:str):
 		""" 文字列を含む配列から数値だけを抽出する """
 		self.df1[Header] = pd.to_numeric(self.df1[Header], errors="coerce")
 
-	def SaveDf(self):
+	def str_to_numeric_all(self):
+		""" すべての行に対して数値だけを抽出する """
+		Header	= list(self.df1.columns)
+		for head in Header:
+			self.df1[head] = pd.to_numeric(self.df1[head], errors="coerce")
+		self.df1.loc[:,:]	= np.array(self.df1, dtype=float)
+
+	def save_df(self):
 		""" csvに保存する """
 		self.df1.to_csv(self.df1)
 
-	def ReturnDf(self):
+	def return_df(self):
 		""" 処理が終わったデータフレームを返す """
 		return self.df1
+
+
+
+	# 複数のデータフレームを編集
+	# -----------------------------------------------------
+	def concat_df(self, FileNames:list, header=True, index_col=0, encoding="utf8"):
+		""" csvを横に連結する """
+		for filename in FileNames:
+			if filename == FileNames[0]:
+				df1	= pd.read_csv(filename, header=header, index_col=index_col, encoding=encoding)
+			else:
+				df2	= pd.read_csv(filename, header=header, index_col=index_col, encoding=encoding)
+				df1	= pd.concat([df1, df2], axis=1)
+		self.df1	= df1
+		return df1
+
+
+
+
+
+
+
+
 
